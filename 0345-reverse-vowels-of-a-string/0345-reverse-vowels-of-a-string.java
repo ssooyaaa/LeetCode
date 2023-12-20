2bc9bfb682218728ad3dea3f33f68077172bcbf5
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-12-20 22:13:57 +0900

    Time: 10 ms (20.27%), Space: 45.4 MB (7.91%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,25 +1,27 @@
 class Solution {
     public String reverseVowels(String s) {
+     
+        int start = 0;
+        int end = s.length()-1;
         
-        List<Integer> listIdx = new ArrayList<>();
-        List<Character> listChar = new ArrayList<>();
+        List<Character> list = Arrays.asList('a','e','i','o','u','A','E','I','O','U');
         
-        StringBuilder sb = new StringBuilder(s);
+        char[] arr = s.toCharArray();
         
-        for(int i=0;i<s.length();i++){
-            if (s.charAt(i) == 'a' ||s.charAt(i) == 'e' ||s.charAt(i) == 'i' ||s.charAt(i) == 'o' ||s.charAt(i) == 'u'|| s.charAt(i) =='A' || s.charAt(i) =='E' || s.charAt(i) =='I' ||s.charAt(i) == 'O' || s.charAt(i) =='U'){
-                listIdx.add(i);
-                listChar.add(s.charAt(i));
+        while(start<end){
+            if(!list.contains(arr[start])) start++;
+            if(!list.contains(arr[end])) end--;
+            
+            if(list.contains(arr[start])&&list.contains(arr[end])){
+                char temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                
+                start++;
+                end--;
             }
         }
         
-        Collections.reverse(listChar);
-        
-        for(int i=0;i<listIdx.size();i++){
-            int idx = listIdx.get(i);
-            sb.replace(idx, idx+1, listChar.get(i).toString());
-        }
-        
-        return sb.toString();
+        return new String(arr);
     }
 }
