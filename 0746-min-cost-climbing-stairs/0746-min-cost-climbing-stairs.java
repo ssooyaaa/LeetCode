713361dfc35d7d3e7c61e41ff4f3afa4d1c3fbb2
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2023-12-31 17:02:55 +0900

    Time: 0 ms (100.00%), Space: 43.3 MB (29.92%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,10 +1,14 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         
+        int[] dp = new int[cost.length+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
         for(int i=2;i<cost.length;i++){
-            cost[i] += Math.min(cost[i-1],cost[i-2]);
+            dp[i] = Math.min(dp[i-1],dp[i-2])+cost[i];
         }
         
-        return Math.min(cost[cost.length-1],cost[cost.length-2]);
+        return Math.min(dp[cost.length-1],dp[cost.length-2]);
     }
 }
