991250be20aed4eaae8f2009793a40c4f1e60de7
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2024-01-23 19:46:38 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (53.14%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+        int i = 0;
+        int k = nums.length;
+        
+        while(i<k){
+            if(nums[i]==val){
+                nums[i] = nums[k-1];
+                k--;
+            }else i++;
+        }
+        
+        return k;
+    }
+}
