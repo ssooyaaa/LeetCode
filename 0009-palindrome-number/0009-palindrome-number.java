4537a865e1d17f2f0683f05462d2e12204cbc9ac
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2024-03-02 23:43:09 +0900

    Time: 6 ms (39.67%), Space: 43.7 MB (89.28%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        String num = Integer.toString(x);
+        int start = 0;
+        int end = num.length()-1;
+        
+        while(start<end){
+            if(num.charAt(start)==num.charAt(end)){
+                start++;
+                end--;
+            }else
+                return false;
+        }
+        
+        return true;
+    }
+}
