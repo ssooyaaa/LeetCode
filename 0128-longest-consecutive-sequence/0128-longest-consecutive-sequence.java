dba9a79ed672b322852761584b55c76e7efa0feb
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2024-03-25 21:22:44 +0900

    Time: 587 ms (31.60%), Space: 57.5 MB (72.04%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -0,0 +1,32 @@
+class Solution {
+        
+    public int longestConsecutive(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+    
+        if(nums.length==0) return 0;
+        
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                if(check(nums[i]-1, map)) map.put(nums[i], map.get(nums[i]-1)+1);
+                else map.put(nums[i], 1);
+            }else {
+                if(check(nums[i]-1, map)) map.put(nums[i], map.get(nums[i]-1)+1);
+            }
+        }
+        
+        int max = Integer.MIN_VALUE;
+        for(int key : map.keySet()){
+            System.out.println(key);
+            if(max<map.get(key)) max = map.get(key);
+        }
+        
+        return max;
+    }
+    
+    public boolean check(int key, Map<Integer, Integer> map){
+        if(map.containsKey(key)) return true;
+        else return false;
+    }
+}
