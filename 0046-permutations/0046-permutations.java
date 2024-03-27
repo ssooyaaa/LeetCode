4d83be724a43b7e05b64f9ed489af6dd2f99e7ab
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2024-03-27 17:53:04 +0900

    Time: 1 ms (95.53%), Space: 44.4 MB (62.23%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,15 +1,16 @@
 class Solution {
-    
     List<List<Integer>> answer = new ArrayList<>();
-    
+        
     public List<List<Integer>> permute(int[] nums) {
         
-        check(nums, new boolean[nums.length], new ArrayList<>());
+        boolean[] visited= new boolean[nums.length];
+        
+        permutation(nums, visited, new ArrayList<>());
         
         return answer;
     }
     
-    public void check(int[] nums, boolean[] visited, List<Integer> list){
+    public void permutation(int[] nums, boolean[] visited, List<Integer> list){
         
         if(list.size()==nums.length){
             answer.add(new ArrayList<>(list));
@@ -17,12 +18,13 @@ class Solution {
         }
         
         for(int i=0;i<nums.length;i++){
-            if(visited[i]) continue;
-            visited[i] = true;
-            list.add(nums[i]);
-            check(nums, visited, list);
-            list.remove(list.size()-1);
-            visited[i] = false;
+            if(!visited[i]){
+                visited[i] = true;
+                list.add(nums[i]);
+                permutation(nums, visited, list);
+                list.remove(list.size()-1);
+                visited[i] = false;
+            }
         }
     }
 }
