3fd5be194fb493feb6e14622e92242d34f32666e
CHOI SOOHYUN <106980524+ssooyaaa@users.noreply.github.com>
2024-05-14 20:59:09 +0900

    Time: 0 ms (100.00%), Space: 41 MB (29.22%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,7 +1,23 @@
 class Solution {
     public int addDigits(int num) {
         
-        return 1+(num-1)%9;
+        if(num<=9) return num;
         
+        int number = num;
+        int sum = 0;
+        
+        while(number>=10){
+            while(number>=1){
+                sum += number%10;
+                number/=10;
+            }
+            
+            if(sum>=10){
+                number = sum;
+                sum = 0;
+            }
+        }
+        
+        return sum;
     }
 }
